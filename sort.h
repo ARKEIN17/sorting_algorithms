bf3e098cd0c27d9cf01daba9133a62dc9070a4ca
ARKEIN17 <ramirez1kevin98@gmail.com>
2022-06-21 21:44:19 -0500

    taks 2

--- sort.h
+++ sort.h
@@ -24,4 +24,5 @@ void print_list(const listint_t *list);
 void insertion_sort_list(listint_t **list);
 listint_t *create_listint(const int *array, size_t size);
 int main(void);
+void selection_sort(int *array, size_t size);
 #endif
