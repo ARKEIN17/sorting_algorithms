1dd211c92112c47f593937c0004977dce4bb782f
xARKEINx <3884@holbertonschool.com>
2022-06-22 14:33:26 -0700

    all

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -11,31 +11,31 @@
 
 int partition(int *array, int start, int end, size_t size)
 {
-    int pivot = array[end];
-    int i = start, j, temp;
+	int pivot = array[end];
+	int i = start, j, temp;
 
-    for (j = start; j < end; j++)
-    {
-        if (array[j] <= pivot)
-        {
-            if (i != j)
-            {
-                temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
-                print_array(array, size);
-            }
-            i++;
-        }
-    }
-    if (i != end)
-    {
-        temp = array[i];
-        array[i] = array[end];
-        array[end] = temp;
-        print_array(array, size);
-    }
-    return (i);
+	for (j = start; j < end; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			if (i != j)
+			{
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+				print_array(array, size);
+			}
+			i++;
+		}
+	}
+	if (i != end)
+	{
+		temp = array[i];
+		array[i] = array[end];
+		array[end] = temp;
+		print_array(array, size);
+	}
+	return (i);
 }
 
 /**
@@ -48,15 +48,14 @@ int partition(int *array, int start, int end, size_t size)
 
 void quickSort(int *array, int start, int end, size_t size)
 {
-    int pivot;
+	int pivot;
 
-    if (start < end)
-    {
-        pivot = partition(array, start, end, size);
-        quickSort(array, start, pivot - 1, size);
-
-        quickSort(array, pivot + 1, end, size);
-    }
+	if (start < end)
+	{
+		pivot = partition(array, start, end, size);
+		quickSort(array, start, pivot - 1, size);
+		quickSort(array, pivot + 1, end, size);
+	}
 }
 /**
  * quick_sort - quick sorts
@@ -65,8 +64,8 @@ void quickSort(int *array, int start, int end, size_t size)
  */
 void quick_sort(int *array, size_t size)
 {
-    int end = size - 1;
-    int start = 0;
+	int end = size - 1;
+	int start = 0;
 
-    quickSort(array, start, end, size);
+	quickSort(array, start, end, size);
 }
